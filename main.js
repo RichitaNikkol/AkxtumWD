6a4423ebfc4c6563e3d6c1e97dfce03f62ed7b3e
Richita.Nikkol <richie.fm@hotmail.com>
2024-03-11 20:52:44 -0600

    mi primer commit

---
+++ main.js
@@ -0,0 +1,9 @@
+ function hidetextbtn(id){
+    let div = document.getElementById(id);
+    if (div.style.display == "none"){
+        div.style.display = "flex";
+    }
+    else {
+        div.style.display = "none"; 
+    }
+ }
